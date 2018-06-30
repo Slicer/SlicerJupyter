fdab169b57a77d540f3561116b715e7f9677598f
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2018-06-29 21:53:51 -0400

    xSlicerServer: Improve event-loop integration using QSocketNotifier to poll sockets
    Suggested-by: Steve Pieper <pieper@bwh.harvard.edu>

--- JupyterKernel/Widgets/xSlicerServer.h
+++ JupyterKernel/Widgets/xSlicerServer.h
@@ -5,23 +5,31 @@
 #include <xeus/xserver_zmq.hpp>
 #include <xeus/xkernel_configuration.hpp>
 
+// Qt includes
+#include <QList>
+#include <QSharedPointer>
+#include <QSocketNotifier>
+
 #include "qSlicerJupyterKernelModuleWidgetsExport.h"
 
 class xSlicerServer : public xeus::xserver_zmq
 {
 
 public:
+    using socket_notifier_ptr = QSharedPointer<QSocketNotifier>;
 
     xSlicerServer(zmq::context_t& context,
                  const xeus::xconfiguration& config);
 
-    virtual ~xSlicerServer() = default;
-
-    void poll_slot();
+    virtual ~xSlicerServer();
 
 protected:
 
     void start_impl(zmq::multipart_t& message) override;
+    void stop_impl() override;
+
+    socket_notifier_ptr make_socket_notifier(const zmq::socket_t& socket, const QString& name);
+    QList< socket_notifier_ptr > m_notifiers;
 };
 
 Q_SLICER_MODULE_JUPYTERKERNEL_WIDGETS_EXPORT
