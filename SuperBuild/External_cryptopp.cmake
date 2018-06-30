1658e4b67cb61acf89b01d050258ef0900fe28f2
Andras Lasso <lasso@queensu.ca>
2018-06-30 06:33:23 +0100

    Fixed Windows build error by disabling assembly

--- SuperBuild/External_cryptopp.cmake
+++ SuperBuild/External_cryptopp.cmake
@@ -66,6 +66,12 @@ if(NOT DEFINED ${proj}_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
     set(cryptopp_disable_sse3 ON)
   endif()
 
+  set(cryptopp_disable_asm OFF)
+  if (WIN32)
+    # Avoid linker errors in Visual Studio
+    set(cryptopp_disable_asm ON)
+  endif()
+
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
     GIT_REPOSITORY "${${CMAKE_PROJECT_NAME}_${proj}_cmake_GIT_REPOSITORY}"
@@ -95,7 +101,7 @@ if(NOT DEFINED ${proj}_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
       -DBUILD_SHARED:BOOL=FALSE
       -DBUILD_STATIC:BOOL=TRUE
       -DDISABLE_SSSE3:BOOL=${cryptopp_disable_sse3}
-      -DDISABLE_ASM:BOOL=OFF
+      -DDISABLE_ASM:BOOL=${cryptopp_disable_asm}
       -Dcryptocpp_DISPLAY_CMAKE_SUPPORT_WARNING:BOOL=OFF
     DEPENDS
       cryptopp-source
