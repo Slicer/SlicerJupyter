b4a95b80ed61cadc935009c232c3fb613bebd087
Andras Lasso <lasso@queensu.ca>
2018-11-27 09:13:36 -0600

    Fix 100% cpu usage
    On most platforms, QSocketNotifier consumes 100% CPU when used on stdin socket, so switch to using a polling timer instead.

--- JupyterKernel/xSlicerServer.cxx
+++ JupyterKernel/xSlicerServer.cxx
@@ -17,45 +17,23 @@
 
 // Qt includes
 #include <QDebug>
-#include <QSocketNotifier>
-#ifdef JUPYTER_POLL_USING_TIMER
 #include <QTimer>
-#endif
 
 xSlicerServer::xSlicerServer(zmq::context_t& context,
                            const xeus::xconfiguration& c)
     : xserver_zmq(context, c)
 {
-#ifdef JUPYTER_POLL_USING_TIMER
   // 50ms interval is sort enough so that users will not notice significant latency
   // yet it is long enough to minimize CPU load caused by polling.
   m_pollTimer = new QTimer();
   m_pollTimer->setInterval(50);
   QObject::connect(m_pollTimer, &QTimer::timeout, [=]() { poll(0); });
-#endif
 }
 
 xSlicerServer::~xSlicerServer()
 {
-#ifdef JUPYTER_POLL_USING_TIMER
   m_pollTimer->stop();
   delete m_pollTimer;
-#endif
-}
-
-xSlicerServer::socket_notifier_ptr
-xSlicerServer::make_socket_notifier(const zmq::socket_t& socket, const QString& name)
-{
-  Q_UNUSED(name);
-
-  socket_notifier_ptr socket_notifier = socket_notifier_ptr(
-        new QSocketNotifier(socket.getsockopt<quintptr>(ZMQ_FD), QSocketNotifier::Read));
-
-  QObject::connect(socket_notifier.data(), &QSocketNotifier::activated, [=](int){
-    poll(0);
-    });
-
-  return socket_notifier;
 }
 
 void xSlicerServer::start_impl(zmq::multipart_t& message)
@@ -67,15 +45,7 @@ void xSlicerServer::start_impl(zmq::multipart_t& message)
 
     m_request_stop = false;
 
-#ifdef JUPYTER_POLL_USING_TIMER
     m_pollTimer->start();
-#else
-    m_notifiers.append(make_socket_notifier(m_shell, "shell"));
-    m_notifiers.append(make_socket_notifier(m_controller, "controller"));
-    m_notifiers.append(make_socket_notifier(m_stdin, "stdin"));
-    m_notifiers.append(make_socket_notifier(m_controller_pub, "controller_pub"));
-    m_notifiers.append(make_socket_notifier(m_publisher_pub, "publisher_pub"));
-#endif
 
     publish(message);
 }
@@ -84,9 +54,7 @@ void xSlicerServer::stop_impl()
 {
   qDebug() << "Stopping Jupyter kernel server";
   this->xserver_zmq::stop_impl();
-#ifdef JUPYTER_POLL_USING_TIMER
   m_pollTimer->stop();
-#endif
   stop_channels();
   std::this_thread::sleep_for(std::chrono::milliseconds(50));
 
--- JupyterKernel/xSlicerServer.h
+++ JupyterKernel/xSlicerServer.h
@@ -12,22 +12,7 @@
 #include <QSharedPointer>
 #include <QSocketNotifier>
 
-// On Windows, socket notifier for stdin socket continuously generates signals,
-// which would cause 100% CPU usage even when the application is idle.
-// It is not clear why stdin socket behaves like this, but using a timer
-// to check for inputs at regular intervals solves the issue.
-// If there are problems with socket notifications on other platforms, too,
-// then polling can be switched to use timer on those platforms as well.
-#if defined(WIN32)
-  #define JUPYTER_POLL_USING_TIMER
-#else
-  #undef JUPYTER_POLL_USING_TIMER
-#endif
-
-
-#ifdef JUPYTER_POLL_USING_TIMER
 class QTimer;
-#endif
 
 class xSlicerServer : public xeus::xserver_zmq
 {
@@ -45,12 +30,12 @@ protected:
     void start_impl(zmq::multipart_t& message) override;
     void stop_impl() override;
 
-    socket_notifier_ptr make_socket_notifier(const zmq::socket_t& socket, const QString& name);
-    QList< socket_notifier_ptr > m_notifiers;
-
-#ifdef JUPYTER_POLL_USING_TIMER
+    // Socket notifier for stdin socket continuously generates signals
+    // on Windows and on some Linux distributions, which would cause 100% CPU
+    // usage even when the application is idle.
+    // It is not clear why stdin socket behaves like this, but using a timer
+    // to check for inputs at regular intervals solves the issue.
     QTimer* m_pollTimer;
-#endif
 };
 
 Q_SLICER_QTMODULES_JUPYTERKERNEL_EXPORT
