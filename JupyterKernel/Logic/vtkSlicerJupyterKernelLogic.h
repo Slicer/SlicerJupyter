b3fba2d929e0265b2e37f627fb96f2563e54f102
Andras Lasso <lasso@queensu.ca>
2021-03-21 23:20:54 -0400

    COMP: Fix build error with VTK9

--- JupyterKernel/Logic/vtkSlicerJupyterKernelLogic.h
+++ JupyterKernel/Logic/vtkSlicerJupyterKernelLogic.h
@@ -43,18 +43,18 @@ public:
 
   static vtkSlicerJupyterKernelLogic *New();
   vtkTypeMacro(vtkSlicerJupyterKernelLogic, vtkSlicerModuleLogic);
-  void PrintSelf(ostream& os, vtkIndent indent) VTK_OVERRIDE;
+  void PrintSelf(ostream& os, vtkIndent indent) override;
 
 protected:
   vtkSlicerJupyterKernelLogic();
   virtual ~vtkSlicerJupyterKernelLogic();
 
-  virtual void SetMRMLSceneInternal(vtkMRMLScene* newScene) VTK_OVERRIDE;
+  virtual void SetMRMLSceneInternal(vtkMRMLScene* newScene) override;
   /// Register MRML Node classes to Scene. Gets called automatically when the MRMLScene is attached to this logic class.
-  virtual void RegisterNodes() VTK_OVERRIDE;
-  virtual void UpdateFromMRMLScene() VTK_OVERRIDE;
-  virtual void OnMRMLSceneNodeAdded(vtkMRMLNode* node) VTK_OVERRIDE;
-  virtual void OnMRMLSceneNodeRemoved(vtkMRMLNode* node) VTK_OVERRIDE;
+  virtual void RegisterNodes() override;
+  virtual void UpdateFromMRMLScene() override;
+  virtual void OnMRMLSceneNodeAdded(vtkMRMLNode* node) override;
+  virtual void OnMRMLSceneNodeRemoved(vtkMRMLNode* node) override;
 
 private:
 
