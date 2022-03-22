Slicer/SlicerJupyter

commit 066b7d9da14cf021b8c49f3f5fb3074452ddc7e9

Update Python kernel to latest version
Slicer's Python version was updated to 3.9, which allowed updating xeus kernel to the latest version:
- xeus 2.3.1
- xeus-python 0.13.6

Everything works except:
- custom display hook: if the custom display hook is modified (sys.displayhook=...) then it has effect in the same cell, but immedately reverted after that. The custom display hook is not essential and most likely will be removed later.
- debugging: it still does not work, the debugger hangs when attach is requested, probably because of control messages are processed in the main thread (https://github.com/jupyter-xeus/xeus-python/issues/275)

SuperBuild/External_xeus.cmake
@@ -20,13 +20,13 @@ if(NOT DEFINED ${proj}_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
 
   ExternalProject_SetIfNotDefined(
     ${CMAKE_PROJECT_NAME}_${proj}_GIT_REPOSITORY
-    "${EP_GIT_PROTOCOL}://github.com/QuantStack/xeus.git"
+    "${EP_GIT_PROTOCOL}://github.com/jupyter-xeus/xeus.git"
     QUIET
     )
 
   ExternalProject_SetIfNotDefined(
     ${CMAKE_PROJECT_NAME}_${proj}_GIT_TAG
-    "0.25.3"
+    "2.3.1"
     QUIET
     )
 

JupyterKernel/qSlicerJupyterKernelModule.cxx
@@ -271,13 +271,20 @@ void qSlicerJupyterKernelModule::startKernel(const QString& connectionFile)
     using history_manager_ptr = std::unique_ptr<xeus::xhistory_manager>;
     history_manager_ptr hist = xeus::make_in_memory_history_manager();
 
+    auto context = xeus::make_context<zmq::context_t>();
+
+    nl::json debugger_config;
+    debugger_config["python"] = QStandardPaths::findExecutable("PythonSlicer").toStdString();
+
     d->Kernel = new xeus::xkernel(d->Config,
                                   "slicer",
+                                  std::move(context),
                                   std::move(interpreter),
-                                  std::move(hist),
-                                  nullptr,
                                   make_xSlicerServer,
-                                  xpyt::make_python_debugger);
+                                  std::move(hist),
+                                  nullptr, // console logger
+                                  xpyt::make_python_debugger,
+                                  debugger_config);
 
     d->Kernel->start();
 

JupyterKernel/xSlicerServer.cxx
@@ -12,16 +12,14 @@
 // zmq includes
 #include <zmq_addon.hpp>
 
-// xeus includes
-#include <xeus/xserver_zmq.hpp>
-
 // Qt includes
 #include <QDebug>
 #include <QTimer>
 
 xSlicerServer::xSlicerServer(zmq::context_t& context,
-                           const xeus::xconfiguration& c)
-    : xserver_zmq(context, c)
+                           const xeus::xconfiguration& c,
+                           nl::json::error_handler_t eh)
+    : xserver_zmq(context, c, eh)
 {
   // 10ms interval is short enough so that users will not notice significant latency
   // yet it is long enough to minimize CPU load caused by polling.
@@ -37,7 +35,7 @@ xSlicerServer::~xSlicerServer()
   delete m_pollTimer;
 }
 
-void xSlicerServer::start_impl(zmq::multipart_t& message)
+void xSlicerServer::start_impl(xeus::xpub_message message)
 {
     qDebug() << "Starting Jupyter kernel server";
 
@@ -48,7 +46,7 @@ void xSlicerServer::start_impl(zmq::multipart_t& message)
 
     m_pollTimer->start();
 
-    publish(message, xeus::channel::SHELL);
+    publish(std::move(message), xeus::channel::SHELL);
 }
 
 void xSlicerServer::stop_impl()
@@ -67,10 +65,11 @@ void xSlicerServer::stop_impl()
   }
 }
 
-std::unique_ptr<xeus::xserver> make_xSlicerServer(zmq::context_t& context,
-                                                  const xeus::xconfiguration& config)
+std::unique_ptr<xeus::xserver> make_xSlicerServer(xeus::xcontext& context,
+                                                  const xeus::xconfiguration& config,
+                                                  nl::json::error_handler_t eh)
 {
-    return std::make_unique<xSlicerServer>(context, config);
+  return std::make_unique<xSlicerServer>(context.get_wrapped_context<zmq::context_t>(), config, eh);
 }
 
 void xSlicerServer::setPollIntervalSec(double intervalSec)

JupyterKernel/xSlicerServer.h
@@ -21,7 +21,8 @@ public:
     using socket_notifier_ptr = QSharedPointer<QSocketNotifier>;
 
     xSlicerServer(zmq::context_t& context,
-                 const xeus::xconfiguration& config);
+                 const xeus::xconfiguration& config,
+                 nl::json::error_handler_t eh);
 
     virtual ~xSlicerServer();
 
@@ -30,7 +31,7 @@ public:
 
 protected:
 
-    void start_impl(zmq::multipart_t& message) override;
+    void start_impl(xeus::xpub_message message) override;
     void stop_impl() override;
 
     // Socket notifier for stdin socket continuously generates signals
@@ -42,7 +43,8 @@ protected:
 };
 
 Q_SLICER_QTMODULES_JUPYTERKERNEL_EXPORT
-std::unique_ptr<xeus::xserver> make_xSlicerServer(zmq::context_t& context,
-                                                  const xeus::xconfiguration& config);
+std::unique_ptr<xeus::xserver> make_xSlicerServer(xeus::xcontext& context,
+                                                  const xeus::xconfiguration& config,
+                                                  nl::json::error_handler_t eh);
 
 #endif

JupyterNotebooks/JupyterNotebooks.py
@@ -27,7 +27,7 @@ The module is originally developed by Andras Lasso.
 
 class SlicerJupyterServerHelper:
   def installRequiredPackages(self, force=False):
-    """Installed required Python packages for running a Jupyter serverin Slicer's Python environment."""
+    """Installed required Python packages for running a Jupyter server in Slicer's Python environment."""
     # Need to install if forced or any packages cannot be imported
     needToInstall = force
     if not needToInstall:
@@ -44,10 +44,7 @@ class SlicerJupyterServerHelper:
     if needToInstall:
       # Install required packages
       import os
-      if os.name=='nt':
-        # There are no official pyzmq wheels for Python-3.6 for Windows, so we have to install manually
-        slicer.util.pip_install("https://files.pythonhosted.org/packages/94/e1/13059383d21444caa16306b48c8bf7a62331ca361d553d2119696ea67119/pyzmq-19.0.0-cp36-cp36m-win_amd64.whl")
-      else:
+      if os.name != 'nt':
         # PIL may be corrupted on linux, reinstall from pillow
         slicer.util.pip_install('--upgrade pillow --force-reinstall')
 

JupyterNotebooks/JupyterNotebooksLib/display.py
@@ -305,7 +305,7 @@ class ViewLightboxDisplay(object):
 
   def _repr_mimebundle_(self, include=None, exclude=None):
     import base64
-    return { self.dataType: base64.b64encode(self.dataValue) }
+    return { self.dataType: base64.b64encode(self.dataValue).decode() }
 
 class MatplotlibDisplay(object):
   """Display matplotlib plot in a notebook cell.
@@ -349,7 +349,7 @@ class MatplotlibDisplay(object):
 
   def _repr_mimebundle_(self, include=None, exclude=None):
     import base64
-    return { self.dataType: base64.b64encode(self.dataValue) }
+    return { self.dataType: base64.b64encode(self.dataValue).decode() }
 
 # Utility functions for customizing what is shown in views
 

JupyterNotebooks/JupyterNotebooksLib/files.py
@@ -113,82 +113,10 @@ def localPath(filename=None):
     return os.path.join(notebookDir, filename)
 
 def notebookPath(verbose=False):
-  """Returns the absolute path of the Notebook or None if it cannot be determined
-
-  .. warning:: works only when the security is token-based and there is also no password.
-
+  """Returns the absolute path of the Notebook.
+  It uses the working directory path where the kernel has started in.
   """
-  # Code is extracted from notebook\notebookapp.py to avoid requiring installation of notebook on server
-
-  import urllib.request
-  import json
-  import io, os, re, sys
-
-  def check_pid(pid):
-
-    def _check_pid_win32(pid):
-      import ctypes
-      # OpenProcess returns 0 if no such process (of ours) exists
-      # positive int otherwise
-      return bool(ctypes.windll.kernel32.OpenProcess(1,0,pid))
-
-    def _check_pid_posix(pid):
-      """Copy of IPython.utils.process.check_pid"""
-      try:
-        os.kill(pid, 0)
-      except OSError as err:
-        if err.errno == errno.ESRCH:
-          return False
-        elif err.errno == errno.EPERM:
-          # Don't have permission to signal the process - probably means it exists
-          return True
-        raise
-      else:
-        return True
-
-    if sys.platform == 'win32':
-      return _check_pid_win32(pid)
-    else:
-      return _check_pid_posix(pid)
-
-  def list_running_servers():
-    # Iterate over the server info files of running notebook servers.
-    # Given a runtime directory, find nbserver-* files in the security directory,
-    # and yield dicts of their information, each one pertaining to
-    # a currently running notebook server instance.
-    runtime_dir = os.path.dirname(connection_file)
-
-    for file_name in os.listdir(runtime_dir):
-      if not re.match('nbserver-(.+).json', file_name):
-        continue
-      if verbose: print(file_name)
-      with io.open(os.path.join(runtime_dir, file_name), encoding='utf-8') as f:
-        info = json.load(f)
-      # Simple check whether that process is really still running
-      if ('pid' in info) and check_pid(info['pid']):
-        yield info
-
-  connection_file = slicer.modules.jupyterkernel.connectionFile
-
-  kernel_id = connection_file.split('-', 1)[1].split('.')[0]
-
-  for srv in list_running_servers():
-    try:
-      if verbose: print(srv)
-      if srv['token']=='' and not srv['password']:  # No token and no password, ahem...
-        req = urllib.request.urlopen(srv['url']+'api/sessions')
-      else:
-        req = urllib.request.urlopen(srv['url']+'api/sessions?token='+srv['token'])
-      sessions = json.load(req)
-      if verbose: print("sessions: "+repr(sessions))
-      for sess in sessions:
-        if sess['kernel']['id'] == kernel_id:
-          return os.path.join(srv['notebook_dir'],sess['notebook']['path'])
-    except Exception as e:
-      if verbose: print("exception: "+str(e))
-      pass  # There may be stale entries in the runtime directory
-
-  return None
+  return slicer.app.startupWorkingPath
 
 def notebookSaveCheckpoint():
     """Save a checkpoint of current notebook. Returns True on success."""

SuperBuild/External_pybind11.cmake
@@ -26,7 +26,7 @@ if(NOT DEFINED ${proj}_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
 
   ExternalProject_SetIfNotDefined(
     ${CMAKE_PROJECT_NAME}_${proj}_GIT_TAG
-    "v2.6.2"
+    "v2.8.1"  # v2.9 did not generate any .sln file, which made the build fail
     QUIET
     )
 

commit 9ec503ccb9d93cf341f7e7bee41a37e3ef467acd

COMP: Fix macOS build error related to xeus "fix_rpath" external project step
This commit fixes a regression introduced in 066b7d9da (Update Python
kernel to latest version). It accounts for xeus binary version update
introduced in jupyter-xeus/xeus@145a0008 (Release 2.1.0).

SuperBuild/External_xeus.cmake
@@ -24,6 +24,8 @@ if(NOT DEFINED ${proj}_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
     QUIET
     )
 
+  # Important: When updating the version of xeus consider also updating
+  #            the "fix_rpath" step below.
   ExternalProject_SetIfNotDefined(
     ${CMAKE_PROJECT_NAME}_${proj}_GIT_TAG
     "2.3.1"
@@ -87,8 +89,12 @@ if(NOT DEFINED ${proj}_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
   set(${proj}_DIR ${CMAKE_BINARY_DIR}/${proj}-build)
 
   if(APPLE)
+     # This corresponds to the XEUS_BINARY_CURRENT value found in the "xeus.hpp" header.
+     # See https://github.com/jupyter-xeus/xeus/blob/master/include/xeus/xeus.hpp
      ExternalProject_Add_Step(${proj} fix_rpath
-       COMMAND install_name_tool -id ${CMAKE_BINARY_DIR}/${Slicer_THIRDPARTY_LIB_DIR}/libxeus.1.dylib ${CMAKE_BINARY_DIR}/${Slicer_THIRDPARTY_LIB_DIR}/libxeus.1.dylib
+       COMMAND install_name_tool -id
+         ${CMAKE_BINARY_DIR}/${Slicer_THIRDPARTY_LIB_DIR}/libxeus.6.dylib
+         ${CMAKE_BINARY_DIR}/${Slicer_THIRDPARTY_LIB_DIR}/libxeus.6.dylib
        DEPENDEES install
        )
   endif()