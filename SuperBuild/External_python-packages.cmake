Slicer/SlicerJupyter

commit c59b08fa9330bae90b48e7df7cc4ff4789cdc911

Fix python package installation
Fixed parso version specification.

Install all python packages at once, as otherwise pip complains when installing pygments that ipython requires jedi and jedi is not found.
Probably ipython requirements appear because ipython was installed in the build tree during manual testing.

SuperBuild/External_python-packages.cmake
@@ -1,4 +1,4 @@
-set(proj python-pygments)
+set(proj python-packages)
 
 # Set dependency list
 set(${proj}_DEPENDENCIES "")
@@ -8,7 +8,7 @@ ExternalProject_Include_Dependencies(${proj} PROJECT_VAR proj DEPENDS_VAR ${proj
 
 if(${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
   ExternalProject_FindPythonPackage(
-    MODULE_NAME "pygments"
+    MODULE_NAME "jedi"
     REQUIRED
     )
 endif()
@@ -20,7 +20,23 @@ endif()
 if(NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
 
   ExternalProject_SetIfNotDefined(
-    ${CMAKE_PROJECT_NAME}_${proj}_VERSION
+    ${CMAKE_PROJECT_NAME}_jedi_VERSION
+    "0.17.0"
+    QUIET
+    )
+
+  # By default, parso-0.8.0 would be downloaded, which is incompatible with
+  # jedi 0.17.x, therefore we need to set parso version manually.
+  ExternalProject_SetIfNotDefined(
+    ${CMAKE_PROJECT_NAME}_parso_VERSION
+    "0.7.1"
+    QUIET
+    )
+
+  # By default, parso-0.8.0 would be downloaded, which is incompatible with
+  # jedi 0.17.x, therefore we need to set parso version manually.
+  ExternalProject_SetIfNotDefined(
+    ${CMAKE_PROJECT_NAME}_pygments_VERSION
     "2.3.1"
     QUIET
     )
@@ -35,18 +51,22 @@ if(NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
 
   set(_no_binary "")
 
-  # Install pygments and requirements
+  # Install jedi and requirements
   # note: --force-reinstall ensures the python dependency is installed within
   #       this library's prefix for packaging.
-  set(_install_pygments COMMAND ${CMAKE_COMMAND}
+  set(_install_jedi COMMAND ${CMAKE_COMMAND}
       -E env
         PYTHONNOUSERSITE=1
         CC=${CMAKE_C_COMPILER}
         PYTHONPATH=${python_sitepackages_DIR}
         ${wrapper_script} ${PYTHON_EXECUTABLE} -m pip install
-          pygments==${${CMAKE_PROJECT_NAME}_${proj}_VERSION} ${_no_binary}
+          jedi==${${CMAKE_PROJECT_NAME}_jedi_VERSION}
+          parso==${${CMAKE_PROJECT_NAME}_parso_VERSION}
+          pygments==${${CMAKE_PROJECT_NAME}_pygments_VERSION}
+          ${_no_binary}
           --prefix ${python_packages_DIR_NATIVE_DIR}
           --force-reinstall
+          --no-warn-script-location
     )
 
   ExternalProject_Add(${proj}
@@ -57,7 +77,7 @@ if(NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
     BUILD_COMMAND ""
     DOWNLOAD_COMMAND ""
     INSTALL_COMMAND ${CMAKE_COMMAND} -E  echo_append ""
-    ${_install_pygments}
+    ${_install_jedi}
     DEPENDS
       ${${proj}_DEPENDENCIES}
     )
@@ -65,7 +85,7 @@ if(NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
   ExternalProject_GenerateProjectDescription_Step(${proj}
     VERSION ${${CMAKE_PROJECT_NAME}_${proj}_VERSION}
     LICENSE_FILES
-      "https://raw.githubusercontent.com/pygments/pygments/master/LICENSE"
+      "https://raw.githubusercontent.com/davidhalter/jedi/master/LICENSE.txt"
     )
 
   #-----------------------------------------------------------------------------

SuperBuild.cmake
@@ -21,8 +21,7 @@ set(${proj}_DEPENDS
    xeus
    xeus-python
    pybind11
-   python-jedi
-   python-pygments
+   python-packages
    )
 
 ExternalProject_Include_Dependencies(${proj}

SuperBuild/External_python-jedi.cmake
@@ -1,96 +1,0 @@
-set(proj python-jedi)
-
-# Set dependency list
-set(${proj}_DEPENDENCIES "")
-
-# Include dependent projects if any
-ExternalProject_Include_Dependencies(${proj} PROJECT_VAR proj DEPENDS_VAR ${proj}_DEPENDENCIES)
-
-if(${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
-  ExternalProject_FindPythonPackage(
-    MODULE_NAME "jedi"
-    REQUIRED
-    )
-endif()
-
-if(NOT DEFINED ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
-  set(${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj} ${Slicer_USE_SYSTEM_python})
-endif()
-
-if(NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
-
-  ExternalProject_SetIfNotDefined(
-    ${CMAKE_PROJECT_NAME}_${proj}_VERSION
-    "0.17.0"
-    QUIET
-    )
-
-  # By default, parso-0.8.0 would be downloaded, which is incompatible with
-  # jedi 0.17.x, therefore we need to set parso version manually.
-  ExternalProject_SetIfNotDefined(
-    ${CMAKE_PROJECT_NAME}_parso_VERSION
-    "0.7.1"
-    QUIET
-    )
-
-  # Alternative python prefix for installing extension python packages
-  set(python_packages_DIR "${CMAKE_BINARY_DIR}/python-packages-install")
-  file(TO_NATIVE_PATH ${python_packages_DIR} python_packages_DIR_NATIVE_DIR)
-
-  set(python_sitepackages_DIR "${CMAKE_BINARY_DIR}/python-packages-install/lib/python3.6/site-packages")
-  file(TO_NATIVE_PATH ${python_sitepackages_DIR} python_sitepackages_DIR_NATIVE_DIR)
-
-
-  set(_no_binary "")
-
-  # Install jedi and requirements
-  # note: --force-reinstall ensures the python dependency is installed within
-  #       this library's prefix for packaging.
-  set(_install_jedi COMMAND ${CMAKE_COMMAND}
-      -E env
-        PYTHONNOUSERSITE=1
-        CC=${CMAKE_C_COMPILER}
-        PYTHONPATH=${python_sitepackages_DIR}
-        ${wrapper_script} ${PYTHON_EXECUTABLE} -m pip install
-          jedi==${${CMAKE_PROJECT_NAME}_${proj}_VERSION} ${_no_binary}
-          parso==${CMAKE_PROJECT_NAME}_parso_VERSION
-          --prefix ${python_packages_DIR_NATIVE_DIR}
-          --force-reinstall
-    )
-
-  ExternalProject_Add(${proj}
-    ${${proj}_EP_ARGS}
-    SOURCE_DIR ${proj}
-    BUILD_IN_SOURCE 1
-    CONFIGURE_COMMAND ""
-    BUILD_COMMAND ""
-    DOWNLOAD_COMMAND ""
-    INSTALL_COMMAND ${CMAKE_COMMAND} -E  echo_append ""
-    ${_install_jedi}
-    DEPENDS
-      ${${proj}_DEPENDENCIES}
-    )
-
-  ExternalProject_GenerateProjectDescription_Step(${proj}
-    VERSION ${${CMAKE_PROJECT_NAME}_${proj}_VERSION}
-    LICENSE_FILES
-      "https://raw.githubusercontent.com/davidhalter/jedi/master/LICENSE.txt"
-    )
-
-  #-----------------------------------------------------------------------------
-  # Launcher setting specific to build tree
-  set(${proj}_PYTHONPATH_LAUNCHER_BUILD
-    ${python_packages_DIR}/${PYTHON_STDLIB_SUBDIR}
-    ${python_packages_DIR}/${PYTHON_STDLIB_SUBDIR}/lib-dynload
-    ${python_packages_DIR}/${PYTHON_SITE_PACKAGES_SUBDIR}
-    )
-  mark_as_superbuild(
-    VARS ${proj}_PYTHONPATH_LAUNCHER_BUILD
-    LABELS "PYTHONPATH_LAUNCHER_BUILD"
-    )
-
-  mark_as_superbuild(python_packages_DIR:PATH)
-
-else()
-  ExternalProject_Add_Empty(${proj} DEPENDS ${${proj}_DEPENDENCIES})
-endif()

commit c9397e905a50546421d577234dd3384266f6e776

COMP: Fix python install to prevent main site-packages corruption
This commit reverts use of --force-reinstall originally introduced
in c59b08fa9 (Fix python package installation).

The use of --force-reinstall led to removal of package like
setuptools from the "site-packages" directory associated
with the application and was causing the build of following
extension (e.g SlicerRadiomics) to fail.

See https://discourse.slicer.org/t/factory-build-of-python-extensions-conflicting-with-each-other/21935

Co-authored-by: James Butler <jbutler@sonovol.com>

SuperBuild/External_python-packages.cmake
@@ -50,8 +50,6 @@ if(NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
   set(_no_binary "")
 
   # Install jedi and requirements
-  # note: --force-reinstall ensures the python dependency is installed within
-  #       this library's prefix for packaging.
   set(_install_jedi COMMAND ${CMAKE_COMMAND}
       -E env
         PYTHONNOUSERSITE=1
@@ -63,7 +61,6 @@ if(NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
           xeus-python-shell==${${CMAKE_PROJECT_NAME}_xeus_python_shell_VERSION}
           ${_no_binary}
           --prefix ${python_packages_DIR_NATIVE_DIR}
-          --force-reinstall
           --no-warn-script-location
     )
 