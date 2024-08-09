a812274e4649cd656ffaebc3fa81ef1f0809a067
Andras Lasso <lasso@queensu.ca>
2024-08-09 10:59:33 -0400

    Fix build errors after xeus update

--- JupyterKernel/xSlicerServer.cxx
+++ JupyterKernel/xSlicerServer.cxx
@@ -16,7 +16,7 @@
 #include <QDebug>
 #include <QTimer>
 
-xSlicerServer::xSlicerServer(zmq::context_t& context,
+xSlicerServer::xSlicerServer(xeus::xcontext& context,
                            const xeus::xconfiguration& c,
                            nl::json::error_handler_t eh)
     : xserver_zmq(context, c, eh)
@@ -26,7 +26,7 @@ xSlicerServer::xSlicerServer(zmq::context_t& context,
   // 50ms causes too long delay in interactive widgets that handle mousemove events.
   m_pollTimer = new QTimer();
   m_pollTimer->setInterval(10);
-  QObject::connect(m_pollTimer, &QTimer::timeout, [=]() { poll(0); });
+  QObject::connect(m_pollTimer, &QTimer::timeout, [=]() { this->poll(); });
 }
 
 xSlicerServer::~xSlicerServer()
@@ -35,24 +35,23 @@ xSlicerServer::~xSlicerServer()
   delete m_pollTimer;
 }
 
-void xSlicerServer::start_impl(xeus::xpub_message message)
+void xSlicerServer::start_impl(xeus::xpub_message msg)
 {
     qDebug() << "Starting Jupyter kernel server";
 
     start_publisher_thread();
     start_heartbeat_thread();
+    set_request_stop(false);
 
-    m_request_stop = false;
+    publish(std::move(msg), xeus::channel::SHELL);
 
     m_pollTimer->start();
-
-    publish(std::move(message), xeus::channel::SHELL);
 }
 
 void xSlicerServer::stop_impl()
 {
   qDebug() << "Stopping Jupyter kernel server";
-  this->xserver_zmq::stop_impl();
+  //this->xserver_zmq::stop_impl();
   m_pollTimer->stop();
   stop_channels();
   std::this_thread::sleep_for(std::chrono::milliseconds(50));
@@ -69,7 +68,7 @@ std::unique_ptr<xeus::xserver> make_xSlicerServer(xeus::xcontext& context,
                                                   const xeus::xconfiguration& config,
                                                   nl::json::error_handler_t eh)
 {
-  return std::make_unique<xSlicerServer>(context.get_wrapped_context<zmq::context_t>(), config, eh);
+  return std::make_unique<xSlicerServer>(context, config, eh);
 }
 
 void xSlicerServer::setPollIntervalSec(double intervalSec)
@@ -80,4 +79,20 @@ void xSlicerServer::setPollIntervalSec(double intervalSec)
 double xSlicerServer::pollIntervalSec()
 {
   return m_pollTimer->interval() / 1000.0;
+}
+
+void xSlicerServer::poll()
+{
+  auto msg = poll_channels(-1);
+  if (msg)
+  {
+    if (msg.value().second == xeus::channel::SHELL)
+    {
+      notify_shell_listener(std::move(msg.value().first));
+    }
+    else
+    {
+      notify_control_listener(std::move(msg.value().first));
+    }
+  }
 }
